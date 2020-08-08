9e6bae75a9239b78a40adfc5021bf81ea875df23
Alex <kerq322@gmail.com>
2020-08-08 04:35:16 +0300

    gf

--- RuVDS_adds.user.js
+++ RuVDS_adds.user.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name         testGit
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       Kerq
+// @downloadURL  https://github.com/Mokerq/test/raw/master/RuVDS_adds.user.js
+// @match        https://ru-admin.10x00.com/*
+// @grant        none
+// ==/UserScript==
 const RuVDS_addons = function () { return this }
 RuVDS_addons.Obj = RuVDS_addons.prototype = {
     show: function (params) {
