bb0cb594c40745b74b8fc06459d463daec6c2ae8
Alex <kerq322@gmail.com>
2020-08-08 04:18:51 +0300

    f

--- RuVDS_adds.meta.js
+++ RuVDS_adds.meta.js
@@ -4,6 +4,8 @@
 // @version      0.2
 // @description  try to take over the world!
 // @author       Kerq
+// @downloadURL  https://raw.githubusercontent.com/Mokerq/test/master/RuVDS_adds.user.js
+// @updateURL  https://raw.githubusercontent.com/Mokerq/test/master/RuVDS_adds.meta.js
 // @match        https://ru-admin.10x00.com/*
 // @grant        none
 // ==/UserScript==
