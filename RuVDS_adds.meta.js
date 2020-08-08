6336929d1c7c62df1715b9731a0fb5624f4dc168
Alex <kerq322@gmail.com>
2020-08-08 04:14:02 +0300

    f

---
+++ RuVDS_adds.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         testGit
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       Kerq
+// @match        https://ru-admin.10x00.com/*
+// @grant        none
+// ==/UserScript==
